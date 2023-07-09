88e02ce46ffe59e04478bc66345a8dd3d5ce6745
Petr Svoboda <svob.petr@seznam.cz>
2023-07-09 15:17:19 +0200

    pridana metoda pro ctení rádku. Nevím jestli neschazí import třídy pro čtení a jestli tak nechat případně první getMapping

--- src/main/java/com/example/demo1/MainController.java
+++ src/main/java/com/example/demo1/MainController.java
@@ -3,9 +3,9 @@ package com.example.demo1;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.io.File;
 @RestController
 public class MainController {
 
@@ -15,15 +15,32 @@ public class MainController {
     File fileScifi=new File("C:\\Users\\svobp\\Desktop\\Open JDK\\ukol lekce 8\\scifi.txt");
 
     Scanner scannerHistoric =new Scanner(fileHistoric);
+
+    while (scannerHistoric.hasNextLine()) {
+        String record =scannerHistoric.nextLine();
+        System.out.println(record);
+    }
     Scanner scannerRomantic=new Scanner(fileRomantic);
+
+    while (scannerRomantic.hasNextLine()) {
+        String record =scannerRomantic.nextLine();
+        System.out.println(record);
+    }
     Scanner scannerScifi=new Scanner(fileScifi);
+
+    while (scannerScifi.hasNextLine()) {
+        String record =scannerScifi.nextLine();
+        System.out.println(record);
+    }
+
+
 
     public MainController() throws FileNotFoundException {
     }
 
     @GetMapping("/historic")
-public File scannerHistoric() {
-    return (fileHistoric);
+    public scannerHistoric.hasNextLine()){
+    return (scannerHistoric.nextLine());
 }
     @GetMapping("/romantic")
     public File scannerRomantic() {
